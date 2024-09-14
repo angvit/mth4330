743d141010bb543e995b46bf26cdb9c1a0b555bf
angvit <angelo.vitalino@baruchmail.cuny.edu>
2024-09-14 17:53:45 -0400

    removed infinity

--- pnorm.py
+++ pnorm.py
@@ -1,22 +1,17 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-p_values = [0.5, 1, 2, 4, np.inf]
+p_values = [0.5, 1, 2, 4, 100]
 theta = np.linspace(0, 2*np.pi, 1000)
 
 plt.figure(figsize=(8, 8))
 
 for p in p_values:
-    if p == np.inf:
-        x = np.sign(np.cos(theta))
-        y = np.sign(np.sin(theta)) 
-    else:
-        x = np.cos(theta) 
-        y = np.sin(theta) 
-        norm = (np.abs(x)**p + np.abs(y)**p) ** (1/p)
-        x /= norm
-        y /= norm
-
+    x = np.cos(theta) 
+    y = np.sin(theta) 
+    norm = (np.abs(x)**p + np.abs(y)**p) ** (1/p)
+    x /= norm
+    y /= norm
     plt.plot(x, y, label=f"p = {p}")
 
 plt.title('Visualizing p-Norms')
@@ -25,6 +20,5 @@ plt.ylabel('$x_2$')
 plt.xlim([-2,2])
 plt.ylim([-2,2])
 plt.legend()
-plt.gca().set_aspect('equal', adjustable='box')
 plt.grid(True)
 plt.show() 
